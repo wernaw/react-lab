16c37b1ce4955124eb7db55c6d9ae57b611d219f
wernaw <werobob@gmail.com>
2025-12-14 12:11:47 +0100

    feat: add movies list

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import {useState} from "react";
 
 function App() {
     const [title, setTitle] = useState('Wall-E');
-
+    const movies = [
+    {title: "Wall-E"},
+    {title: "Pulp Fiction"},
+    {title: "Matrix"},
+    {title: "1670"},
+];
     let message;
     if (title.length < 4) {
         message = "Title is too short.";
@@ -18,6 +23,9 @@ function App() {
   return (
       <div>
           <h1>My favourite movies to watch</h1>
+          <ul>
+              {movies.map((movie) => <li key={movie.title}>{movie.title}</li>)}
+          </ul>
           <h2>My favourite movie for today is {title}</h2>
           <p1>{title.length > 0 && <div>{message}</div>}</p1>
           <input type="text" value={title} onChange={(event) => setTitle(event.target.value)}/>
